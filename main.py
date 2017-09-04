af1aaea43e0c28a9c0bb09818633f548d0b15b1d
lasaly <lasaly@gmail.com>
2017-09-03 18:37:03 -0600

    build-a-blog is functional

--- main.py
+++ main.py
@@ -1,21 +1,68 @@
 from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql=pymysql://build-a-blog:PrepWork9@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:debugme@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class blog(db.Model):
+class Blog(db.Model):     #specific class extends object db and inherits functionality from Model class
+    
+    id = db.Column(db.Integer, primary_key=True) # id will function as a primary key and will associate with a specific column
+    title = db.Column(db.String(80)) # a column called title will be created in sql
+    body = db.Column(db.Text) # a column called body will be created in sql 
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(550))
-
-    def __init__(self, title):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
 
 
+@app.route('/')
+def index():
+    return redirect('/blog')   
+
+
+@app.route('/blog', methods=["POST", "GET"])
+def blog():
+
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        title_error, body_error = "", ""   #tuples 
+
+        if not title: title_error = "What do you want to ruminate about?"
+        if not body: body_error = "Please share your thoughts here."
+
+        if not title_error and not body_error: 
+            new_blog = Blog(title, body)
+            db.session.add(new_blog)
+            db.session.commit()
+            
+            return redirect('/blog?id='+str(new_blog.id))
+
+        else: 
+            return render_template('/newpost.html', page_header="Something to Ruminate About", title_error=title_error, 
+            body_error=body_error, title=title, body=body)
+
+    if request.args.get('id'):        
+        blog_id = int(request.args.get('id'))
+        blog = Blog.query.get(blog_id)
+
+        if blog: 
+            return render_template('/indiv_post.html', page_header="A Fresh Rumination", blog=blog)
+        else: 
+            return render_template('no_blog.html', page_header="Missing Thoughts")
+
+    blogs = Blog.query.all()
+    return render_template('/blog.html', page_header="All Blogs", blogs=blogs)
+
+
+@app.route('/newpost')
+def new_entry(): 
+    return render_template('/newpost.html', page_header="Something to Ruminate About")
+
+if __name__ == '__main__':
+    app.run()
